2a81748cebf3a5af383511f8584b2f19aa30326d
Hsiamomomo <frazier.hsia@gmail.com>
2023-02-14 13:17:22 +0000

    2

--- README.md
+++ README.md
@@ -1,39 +1,70 @@
+# Getting Started with Create React App
 
-[![Mui Badge](https://img.shields.io/badge/Material%20UI-007FFF?style=for-the-badge&logo=mui&logoColor=white)](#)
-[![React Badge](https://img.shields.io/badge/-React-61DBFB?style=for-the-badge&labelColor=black&logo=react&logoColor=61DBFB)](#)
-[![Javascript Badge](https://img.shields.io/badge/-Javascript-F0DB4F?style=for-the-badge&labelColor=black&logo=javascript&logoColor=F0DB4F)](#)
-[![Javascript Badge](https://img.shields.io/badge/-EMAIL.JS-green?style=for-the-badge)](#)
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+## Available Scripts
 
-# Personal Portfolio
+In the project directory, you can run:
 
-## Description 
-Welcome to my personal portfolio website! This is a SPA (Single Page Application) built with REACT, showcasing my skills and experience as a developer.
+### `yarn start`
 
-On the portfolio page, you can find more information about me, including my education and work experience. You can also download my resume and see some of my recent projects. Additionally, there is a contact form for you to get in touch with me.
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
-:point_right: [Go to Portfolio web page](https://aleksandrriabov.github.io/portfolio/#/portfolio/) :point_left:
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
 
-## Features
-- Personal information and resume
-- Project showcase
-- Contact form
+### `yarn test`
 
-## Installation
-  1. Clone the repository: **`git clone https://github.com/AleksandrRiabov/portfolio.git`**
-  2. Navigate to cloned directory using **`cd portfolio`** command.
-  3. Open index.html file from terminal, using **`open index.html`** command.
+Launches the test runner in the interactive watch mode.\
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
-## Screenshot
-![alt text](https://user-images.githubusercontent.com/61385379/213051961-db19b4ac-dfde-4ffd-855c-6dd5453becf5.png)
+### `yarn build`
 
-## Live Demo
-:point_right: [Go to Portfolio web page](https://aleksandrriabov.github.io/portfolio/#/portfolio/) :point_left:
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
 
-## Built With
-- **REACT** - A JavaScript library for building user interfaces.
-- **Material-UI** - A popular React UI framework that implements Google's Material Design guidelines.
-- **email.js** - A JavaScript library for sending email from the browser.
+The build is minified and the filenames include the hashes.\
+Your app is ready to be deployed!
 
-## Credits
-[Aleksandr Riabov](https://github.com/AleksandrRiabov) :clap:
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+
+### `yarn eject`
+
+**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+
+If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+
+Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+
+You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+
+## Learn More
+
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+
+To learn React, check out the [React documentation](https://reactjs.org/).
+
+### Code Splitting
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
+
+### Analyzing the Bundle Size
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
+
+### Making a Progressive Web App
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
+
+### Advanced Configuration
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
+
+### Deployment
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
+
+### `yarn build` fails to minify
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/data.js
+++ src/data.js
@@ -1,113 +1,189 @@
-import moviesSmall from './images/movies/moviesSmall.png';
-import moviesMedium from './images/movies/moviesMedium.jpg';
-import moviesLarge from './images/movies/moviesLarge.jpg';
-import moviesXL from './images/movies/moviesXL.png';
-
-import pToolSmall from './images/pTool/pToolSmall.png';
-import pToolMedium from './images/pTool/pToolMedium.jpg';
-import pToolLarge from './images/pTool/pToolLarge.jpg';
-import pToolXL from './images/pTool/pToolXL.png';
-
-import GitHub from '@mui/icons-material/GitHub';
-import LinkedIn from '@mui/icons-material/LinkedIn';
-
-
-//Portfolio => desc Recomended 300 characters.
-
-const data = {
-  portfolio: [
+export const featuredPortfolio = [
     {
-      name: 'export-planning-tool',
-      title: 'Export Planning Tool',
-      subtitle:
-        'Designed to simplify the process of estimating the required number of trailers.',
-      featured: true,
-      url: 'https://guarded-savannah-03210.herokuapp.com/',
-      github: 'https://github.com/AleksandrRiabov/fs-planning-tool',
-      technologies: [
-        'React.Js',
-        'Node.Js (Express)',
-        'MongoDB Atlas',
-        'MaterialUI',
-        'Charts.Js',
-        'Moment.Js',
-        'AJAX',
-        'react-router',
-        'Deploy on Heroku',
-      ],
-      imgSmall: pToolSmall,
-      imgMedium: pToolMedium,
-      imgLarge: pToolLarge,
-      imgXl: pToolXL,
-      desc: "MERN stack Application designed to simplify the process of estimating the required number of trailers. All estimates are based on the average of the five previous weeks, or specific days of the week. With app's charts, you can easily identify seasonal orders spikes, and be prepared for the growing demand.",
-      paragraphs: [
-        'Some of the warehouses hiring other logistic companies to distribute the stock. To speed up deliveries it is beneficial to request trailers as soon as the warehouse received the order. However, it is very difficult to predict the number of trailers before the warehouse order has been fully picked and the number of pallets available. There is always a risk to request too many or not enough trailers to distribute everything. This App will help to make all estimates more accurate.',
-      ],
+      id: 1,
+      title: "Social Media App",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
     },
     {
-      name: 'movies',
-      title: 'Movies App',
-      subtitle: 'Ract App which use a real API to display movies.',
-      featured: false,
-      url: 'https://aleksandrriabov.github.io/movies/',
-      github: 'https://github.com/AleksandrRiabov/movies',
-      technologies: [
-        'ReactJs',
-        'Responsive CSS',
-        'MaterialUI',
-        'AJAX',
-        'GitHub',
-        'react-router-dom',
-      ],
-      imgSmall: moviesSmall,
-      imgMedium: moviesMedium,
-      imgLarge: moviesLarge,
-      imgXl: moviesXL,
-      desc: 'This App allows you to view popular, now playing and top rated movies. View Individual movie and get the movies data such as cast, movie duration, summary, recommended movies,etc. View Individual Persons and get thier details. Search for Movies using search bar.',
-      paragraphs: [
-        "Application consuming themoviedb.org API. Some of the pages design is cloned from API provider's website.",
-      ],
-    },
-  ],
-  socialLinks: [
-    {
-      name: 'GitHub',
-      icon: <GitHub fontSize='inherit' />,
-      url: 'https://github.com/AleksandrRiabov',
-      message: 'Follow for open-source projects',
+      id: 2,
+      title: "Rampa UI Design",
+      img:
+        "https://cdn.dribbble.com/users/702789/screenshots/15054318/media/4ea5d492b7b07eebc9528ff960794879.png?compress=1&resize=1200x900",
     },
     {
-      name: 'LinkedIn',
-      icon: <LinkedIn fontSize='inherit' />,
-      url: 'https://www.linkedin.com/in/aleksandr-riabov',
-      message: "Let's connect on LinkedIn",
-    },
-  ],
-  links: [
-    {
-      name: 'Home',
-      url: '/',
-      navLink: true,
+      id: 3,
+      title: "E-commerce Web Design",
+      img:
+        "https://cdn.dribbble.com/users/1387827/screenshots/15466426/media/deb2dca6762cd3610321c98bfccb0b72.png?compress=1&resize=1200x900",
     },
     {
-      name: 'Portfolio',
-      url: '/portfolio',
-      message: "View Alek's previous work",
-      navLink: true,
+      id: 4,
+      title: "Relax Mobile App",
+      img:
+        "https://cdn.dribbble.com/users/4095861/screenshots/15467417/media/d6a15c416626f12b31fa5ca1db192572.png?compress=1&resize=1200x900",
     },
     {
-      name: 'About',
-      url: '/about',
-      message: "Learn about Alek's skills",
-      navLink: true,
+      id: 5,
+      title: "Hero Web Design",
+      img:
+        "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
     },
     {
-      name: 'Get In Touch',
-      url: '/contacts',
-      message: 'Send a general message',
-      navLink: false,
+      id: 6,
+      title: "Banking App",
+      img:
+        "https://cdn.dribbble.com/users/3307260/screenshots/15468444/media/447b4501f7a145b05e11c70199a9c417.jpg?compress=1&resize=1200x900",
     },
-  ],
-};
-
-export default data;
+  ];
+  
+  export const webPortfolio = [
+    {
+      id: 1,
+      title: "Web Social Media App",
+      img: "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
+    },
+    {
+      id: 2,
+      title: "Web Rampa UI Design",
+      img: "https://cdn.dribbble.com/users/3967258/screenshots/15463803/media/4fddb9a2caf3b3bd634060f706a91e73.png?compress=1&resize=1200x900",
+    },
+    {
+      id: 3,
+      title: "Web E-commerce Design",
+      img: "https://cdn.dribbble.com/users/3637068/screenshots/15467010/media/69a3279182b75365013fe285733d1122.png?compress=1&resize=1200x900",
+    },
+    {
+      id: 4,
+      title: "Web Relax App",
+      img: "https://cdn.dribbble.com/users/2264844/screenshots/15463998/media/d85e92a332c41094f68e1f716884f7ce.jpg?compress=1&resize=1200x900",
+    },
+    {
+      id: 5,
+      title: "Web Design",
+      img: "https://cdn.dribbble.com/users/387658/screenshots/15458608/media/e92ec1dd33dd1170ae3dc9c5272f2693.jpg?compress=1&resize=1200x900",
+    },
+    {
+      id: 6,
+      title: "Web Banking App",
+      img: "https://cdn.dribbble.com/users/6384483/screenshots/15468426/media/ce9479fa1f8dba3a4a49840d76e55e31.png?compress=1&resize=1200x900",
+    },
+  ];
+  
+  export const mobilePortfolio = [
+    {
+      id: 1,
+      title: "Mobile Social Media App",
+      img:
+        "https://cdn.dribbble.com/users/2597268/screenshots/15468893/media/44313553d73ba41580f1df69749bba28.jpg?compress=1&resize=1200x900",
+    },
+    {
+      id: 2,
+      title: "Mobile Rampa UI Design",
+      img:
+        "https://cdn.dribbble.com/users/5920881/screenshots/15463142/media/b5a460223798bd85d835710806e2f3dd.png?compress=1&resize=1200x900",
+    },
+    {
+      id: 3,
+      title: "Mobile E-commerce Design",
+      img:
+        "https://cdn.dribbble.com/users/1998175/screenshots/15459384/media/48ac2b43ebe81ba0866afea1383cc939.png?compress=1&resize=1200x900",
+    },
+    {
+      id: 4,
+      title: "Mobile Relax App",
+      img:
+        "https://cdn.dribbble.com/users/2083704/screenshots/15468619/media/cd958306c7a772449e1ac23bd65ce506.png?compress=1&resize=1200x900",
+    },
+    {
+      id: 5,
+      title: "Mobile Hero Design",
+      img:
+        "https://cdn.dribbble.com/users/26642/screenshots/15462545/media/1a202ef5c8338b6f78634e6edc1ba350.png?compress=1&resize=1200x900",
+    },
+    {
+      id: 6,
+      title: "Mobile Banking App",
+      img:
+        "https://cdn.dribbble.com/users/1387827/screenshots/15466426/media/deb2dca6762cd3610321c98bfccb0b72.png?compress=1&resize=1200x900",
+    },
+  ];
+  
+  export const designPortfolio = [
+    {
+      id: 1,
+      title: "Design Social Media App",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 2,
+      title: "Design Rampa UI Design",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 3,
+      title: "Design E-commerce Web Design",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 4,
+      title: "Design Relax Mobile App",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 5,
+      title: "Design Keser Web Design",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 6,
+      title: "Design Banking App",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+  ];
+  
+  export const contentPortfolio = [
+    {
+      id: 1,
+      title: "Content Social Media App",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 2,
+      title: "Content Rampa UI Design",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 3,
+      title: "Content E-commerce Web Design",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 4,
+      title: "Content Relax Mobile App",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 5,
+      title: "Content Keser Web Design",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 6,
+      title: "Content Banking App",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+  ];
+  
