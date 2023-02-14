5b8a3f5fba3324e2a9f6d2301e9f8c69de8742a8
Hsiamomomo <frazier.hsia@gmail.com>
2023-02-14 13:32:54 +0000

    3

---
+++ .history/index_20230214133239.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Index from './index';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
